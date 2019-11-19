fdbe90be05c3fb7949c060fed260b8d67b9fd705
Alex Davidoff <ad35@pdx.edu>
2019-11-19 08:47:55 -0800

    finished most of assignment 5, need to clean up files

--- Assignment5/app.js
+++ Assignment5/app.js
@@ -1,47 +1,22 @@
-var createError = require('http-errors');
 var express = require('express');
-var body = require('body-parser');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-
+var bodyParser = require('body-parser');
 var app = express();
+var path = require('path');
+var _dirname = "/u/ad35/cs465-565assignments/Assignment5";
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+//app.set('view engine', 'pug');
+//app.set('views', './views');
 
-app.use(logger('dev'));
-app.use(express.json());
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', function (req, res) {
-  res.render('index');
+app.get('/', function(req, res) {
+  res.sendFile(path.join(_dirname + "/index.html"));
 });
 
-app.post('/submit', function (req, res) {
-  res.send('name: ' + req.body.firstname + '\nemail: ' + req.body.email
-      + '\ncomment: ' + req.body.comments);
-});
+app.post('/submit', function(req, res) {
+	res.send('name: ' + req.body.firstname + '\nemail: ' + req.body.email + '\ncomments: ' + req.body.comments);
+});	
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
+//app.listen(8080);
 module.exports = app;
