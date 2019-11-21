fae7c64384978a59d445238f862ad67a12b490ae
Alex Davidoff <ad35@pdx.edu>
2019-11-21 13:00:24 -0800

    completed assignment 5 exercise 3 form

--- Assignment5/app.js
+++ Assignment5/app.js
@@ -2,21 +2,15 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 var path = require('path');
-var _dirname = "/u/ad35/cs465-565assignments/Assignment5";
 
-//app.set('view engine', 'pug');
-//app.set('views', './views');
-
-app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 
 app.get('/', function(req, res) {
-  res.sendFile(path.join(_dirname + "/index.html"));
+  res.sendFile(path.join(__dirname + "/index.html"));
 });
 
 app.post('/submit', function(req, res) {
-	res.send('name: ' + req.body.firstname + '\nemail: ' + req.body.email + '\ncomments: ' + req.body.comments);
+	res.send('name: ' + req.body.firstname + '<br />email: ' + req.body.comments + '<br />comments: ' + req.body.comments);
 });	
 
-//app.listen(8080);
-module.exports = app;
+app.listen(8080);
